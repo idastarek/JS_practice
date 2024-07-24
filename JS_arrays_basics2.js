e4dad67e4e73b9004d021d2c3f2f92b4ecd58b11
Ida <ida.starek@gmail.com>
2024-07-24 21:43:18 +0100

    reduce()

--- JS_arrays_basics2.js
+++ JS_arrays_basics2.js
@@ -48,3 +48,42 @@ const menuItems = [
   // what's different about reduce() is the way it iterates over each element
   // unlike the other array methods, where we just got the element itself, before we get each element in the parameters of callback, we have a special value called an accumulator
   
+  menuItems.reduce((accumulator, menuItem) => { 
+    return accumulator;
+}, 0);
+
+// what's different about reduce() and the function that is passed to it is that it always returns the accumulator instead of the element itself
+
+// accumulator is a storage for whatever value you want to hold in it
+// reduce() is going to remember the value of the accumulator every time the function runs
+
+// for the accumulator to work and store new values, it has to be returned every time the function runs
+
+// first we always need to return the accumulator, then we want to get the price of each menu item that we're iterating over and we want to put it on the accumulator
+
+const total = menuItems.reduce((accumulator, menuItem) => {
+    console.log(`
+        accmulator: ${accumulator},
+        menu item price: ${menuItem.price}
+        `);
+        return accumulator + menuItem.price;
+}, 0);
+
+console.log(total);
+
+// virtually, every major array can be created with .reduce()
+
+// multiply each num in the array by 2, using .reduce()
+
+const numbers = [1, 2, 3, 4, 5, 6];
+ const number_doubled = numbers.reduce((acc, num) => {
+     acc.push(num*2);
+     return acc;
+ }, []);
+ 
+ // vs using .map()
+ const number_doubled2 = numbers.map(num => num*2);
+ 
+ console.log(number_doubled2);
+
+ 
